92f986c6bfc98679d755767ac6b6d1dbdb922cd3
jordasche <80426378+jordasche@users.noreply.github.com>
2021-11-28 21:56:09 -0500

    Adding all files

---
+++ language-exercise.js
@@ -0,0 +1,10 @@
+// let language = "FR";
+let language = "EN";
+
+if (language === "EN") {
+   console.log("English");
+} else if (language === "FR") {
+   console.log("French");
+} else {
+   console.log("Unknown Language");
+}
